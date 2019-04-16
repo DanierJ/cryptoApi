7eee130fcfdf4d5895e5f63f900d05c3d7ce027b
DanierJ <danierjavid@gmail.com>
2019-04-16 12:10:51 -0500

    Use request module by fetching data from api - object destructuration

--- js/main.js
+++ js/main.js
@@ -19,8 +19,23 @@ app.post('/', (req, res) => {
    const crypto = req.body.crypto,
          fiat = req.body.fiat;
 
-   request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', (err, res, body) => {
-       console.log(res.body);
+   /// Requesting to an external api
+   request('https://apiv2.bitcoinaverage.com/indices/global/ticker/' + crypto + fiat, (err, response, body) => {
+       const bitInfo = JSON.parse(response.body),
+             currentDate = bitInfo.display_timestamp;/*,
+             {open} = bitInfo,
+             {hour} = open;
+       console.log(open);
+       console.log(hour);*/
+
+       // For more than one response to browser
+       res.write(`<p>The current date is ${currentDate}</p>`);
+
+       // Last thing you wanna do
+       res.write(`<h1>The current price of ${crypto} the bitcoin is ${bitInfo.last} ${fiat}`);
+
+       res.send();
+
    });
 
 });
